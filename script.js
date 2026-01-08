d9aede587ce094292a30659bc1b0991176e85ee0
AlaraRivka <alara.broekema@student.howest.be>
2026-01-08 11:05:51 +0100

    Update display methode

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 
+// INPUT ELEMENTEN
+
+
 const hexcolour = document.getElementById("colourpicker");
 const stylepicker = document.getElementById("styles");
-const btnEl = document.getElementById("startbutton");
-const displayp = document.getElementById("displayhex");
-const copybutton = document.getElementById("copykleuren");
-copybutton.disabled = true;
 
-const btnSlaKleurOpBestaandeGebruiker = document.getElementById("slaitemsop2")
 
-const btnReset = document.getElementById("btnreset")
-
-const dropdownGebruikersEl = document.getElementById("gebruikers")
-const toonGebruikersDataParagraafEL = document.getElementById("toongebruikersdata");
-const btnHaalDataOp = document.getElementById("btnhaaldataop");
 const nameInputEl = document.getElementById("usernameinput")
 const kleurenschemainputnaamEl = document.getElementById("kleurensetnaam")
+
+//DISPLAY ELEMENTEN
+
+const displayp = document.getElementById("displayhex");
+const toonGebruikersDataParagraafEL = document.getElementById("toongebruikersdata");
+
+
+//AL MIJN KNOPPEN
+
+const btnSlaKleurOpBestaandeGebruiker = document.getElementById("slaitemsop2")
+const btnReset = document.getElementById("btnreset")
+
+const btnEl = document.getElementById("startbutton");
+
+const btnHaalDataOp = document.getElementById("btnhaaldataop");
+
+const dropdownGebruikersEl = document.getElementById("gebruikers")
 const btnSlaItemsOp = document.getElementById("slaitemsop");
+
+const copybutton = document.getElementById("copykleuren");
+copybutton.disabled = true;
 
 let divElC1 = document.getElementById("colour1");
 let divElC2 = document.getElementById("colour2");
@@ -23,23 +36,27 @@ let divElC3 = document.getElementById("colour3");
 let divElC4 = document.getElementById("colour4");
 let divElC5 = document.getElementById("colour5");
 
+let myArrayofDivColourDisplay = Array.from(document.getElementsByClassName("hexdisplaywindow")) // HIER SLA IK ALLE DIVS VOOR KLEUR SCHEMA IN OP, dit toont kleuren, maar slaat ze ook op....
 
 renderGebruikersLijst();
 
-copybutton.addEventListener("click", function () {
+// --------------- HIER ZIJN MIJN EVENTLISTENERS ------------------------------- //
+// --------------- HIER ZIJN MIJN EVENTLISTENERS ------------------------------- //
 
+
+
+copybutton.addEventListener("click", function () {
     // Copy the text inside the text field
     navigator.clipboard.writeText(displayp.textContent);
     // Alert the copied text
     alert("Copied the text: " + displayp.textContent);
 });
 
-btnEl.addEventListener("click", fetchAndDisplayColourScheme);
-btnSlaItemsOp.addEventListener("click", SlaGegevensOp);
+btnEl.addEventListener("click", fetchAndDisplayColourSchemeFromApi);
+btnSlaItemsOp.addEventListener("click", SlaGegevensOpInLocaleStorage);
 btnHaalDataOp.addEventListener("click", haalMijnDataOpEnToonHetaanDeGebruiker);
 
 btnSlaKleurOpBestaandeGebruiker.addEventListener("click", slaKleurenOpVoorBestaandeGebruiker)
-
 
 btnReset.addEventListener("click", function () {
 
@@ -50,10 +67,15 @@ btnReset.addEventListener("click", function () {
     renderGebruikersLijst();
 });
 
+
+// --------------- HIER ZIJN MIJN FUNCTIES------------------- //
+// --------------- HIER ZIJN MIJN FUNCTIES------------------- //
+
+
 function slaKleurenOpVoorBestaandeGebruiker() {
 
-    let key = dropdownGebruikersEl.value
-    let myObject = JSON.parse(localStorage.getItem(key));
+    let key = dropdownGebruikersEl.value //haalt de key op(dit is de gebruikernaams)
+    let myObject = JSON.parse(localStorage.getItem(key)); //nu heb ik het object
 
     if (displayp.textContent === "") {
         alert("Kus eerst een set kleuren!");
@@ -63,11 +85,13 @@ function slaKleurenOpVoorBestaandeGebruiker() {
     }
     else {
 
+
+        myObject.colourschemeinhexarray.clear
+        matchMedia.colourschemeinhexarray
         myObject.arrayofallschemeinhex.push(displayp.textContent);
         myObject.arrayofallcolourschemesnames.push(kleurenschemainputnaamEl.value)
     }
 
-    localStorage.removeItem(key);
     localStorage.setItem(key, JSON.stringify(myObject));
 
     haalMijnDataOpEnToonHetaanDeGebruiker();
@@ -88,7 +112,7 @@ function renderGebruikersLijst() {
 
 }
 
-function haalMijnDataOpEnToonHetaanDeGebruiker() {
+function haalMijnDataOpEnToonHetaanDeGebruiker() { // DIT HAALT DE DATA DAT IS OPGESLAGEN IN DE LOCAL STORAGE OP EN TOONT HET AAN DE GEBRUIKER
 
     let setarrayofcolours = ""
     let mykey = dropdownGebruikersEl.value;
@@ -111,17 +135,17 @@ function haalMijnDataOpEnToonHetaanDeGebruiker() {
 
 
 
-function SlaGegevensOp() {
+function SlaGegevensOpInLocaleStorage() {  // DIT SLAAT DE GEBRUIKERS GEGEVENS OP EN KLEURENSCHEMA OP IN EEN JSON OBJECT IN DE LOCALE STORAGE DATABASE
 
     let MyStorageobject =
     {
         usernamestring: "",
 
-        colourschemenamestring: "",
-        arrayofallcolourschemesnames: [],
+        colourSchemeNameString: "", // Naam dat de gebruiker geeft aan  de kleuren schema
+        arrayOfAllColourSchemesNames: [], // Een array van alle namen van kleurenschemas
 
-        colourschemeinhexstring: "",
-        arrayofallschemeinhex: [],
+        colourSchemeInHexArray: [], //Dit is een array met 9 hex codes
+        arrayOfAllColourSchemes: [], // Dit is een array van arrays 
 
         TijdVanOpslag: Date.now()
     }
@@ -137,10 +161,14 @@ function SlaGegevensOp() {
         let arrayposition = 0;
 
         MyStorageobject.usernamestring = nameInputEl.value;
-        MyStorageobject.colourschemenamestring = kleurenschemainputnaamEl.value;
-        MyStorageobject.colourschemeinhexstring = displayp.textContent;
-        MyStorageobject.arrayofallschemeinhex.push(MyStorageobject.colourschemeinhexstring)
-        MyStorageobject.arrayofallcolourschemesnames.push(MyStorageobject.colourschemenamestring);
+        MyStorageobject.colourSchemeNameString = kleurenschemainputnaamEl.value;
+
+        MyStorageobject.colourSchemeInHexArray
+
+        MyStorageobject.arrayOfAllColourSchemes.push(MyStorageobject.colourSchemeInHexArray);
+
+        MyStorageobject.arrayOfAllColourSchemes.push(MyStorageobject.colourschemeinhexstring)
+
 
         console.log(`Hey ${MyStorageobject.usernamestring} !
     \n Je sloeg de kleuren schema op met de naam: ${MyStorageobject.colourschemenamestring}.
@@ -159,35 +187,58 @@ function SlaGegevensOp() {
 
 };
 
-function fetchAndDisplayColourScheme() {
+function fetchAndDisplayColourSchemeFromApi() { // DIT IS MIJN FETCH METHODE HIER HAAL IK DE DATA OP VAN DE API< WANNEER GEBRUIKER KLEUR KIEST
 
     let stylepickerEl = stylepicker.value;
     let hexcolourvalue = hexcolour.value;
     let hexcolourvaluepicked = hexcolourvalue.slice(1);
 
+    let hexdisplaytextfragment = ''
+    let hexdisplaytextfull = ''
+
     let arrayofcolours = fetch(`https://www.thecolorapi.com/scheme?hex=${hexcolourvaluepicked}&mode=${stylepickerEl}&format=json&count=6`)
         .then(response => response.json())
         .then(data => {
 
-            let c1 = data.colors[0].hex.value;
-            console.log(typeof c1)
-            let c2 = data.colors[1].hex.value;
-            let c3 = data.colors[2].hex.value;
-            let c4 = data.colors[3].hex.value;
-            let c5 = data.colors[4].hex.value;
+            console.log(`Items in de kleuren array van de api ${data.colors.length}`)
+            console.log(`Items in mijn div collectie ${myArrayofDivColourDisplay.length}`)
 
-            divElC1.style.backgroundColor = c1;
-            divElC2.style.backgroundColor = c2;
-            divElC3.style.backgroundColor = c3;
-            divElC4.style.backgroundColor = c4;
-            divElC5.style.backgroundColor = c5;
-            console.log(c1);
+            let arrayofcolorstemp = []
 
-            let displayEl = `${c1} || ${c2} || ${c3}  || ${c4} || ${c5}`
-            displayp.textContent = displayEl;
+            for (let i = 0; i < data.colors.length; i++) {
+
+                myArrayofDivColourDisplay[i].style.backgroundColor = data.colors[i].hex.value;
+                hexdisplaytextfragment = `|${data.colors[i].hex.value} `
+                hexdisplaytextfull += hexdisplaytextfragment
+
+            }
+            console.log(`Items in de array die maak als test en de data van api in plaats ${arrayofcolours.length}`)
+            console.log(`Display text die ik maak van strings ${hexdisplaytextfull}`)
+            console.log(`Kleur van de picked getrimd : ${hexcolourvaluepicked}`)
+            displayp.textContent = hexdisplaytextfull;
             copybutton.disabled = false;
 
+
+            // let c1 = data.colors[0].hex.value;
+            // let c2 = data.colors[1].hex.value;
+            // let c3 = data.colors[2].hex.value;
+            // let c4 = data.colors[3].hex.value;
+            // let c5 = data.colors[4].hex.value;
+
+            // divElC1.style.backgroundColor = c1;
+            // divElC2.style.backgroundColor = c2;
+            // divElC3.style.backgroundColor = c3;
+            // divElC4.style.backgroundColor = c4;
+            // divElC5.style.backgroundColor = c5;
+            // console.log(c1);
+
+            // let hexdisplaytext = `${c1} || ${c2} || ${c3}  || ${c4} || ${c5}` // toont de kleuren in hex codes in text 
+            // displayp.textContent = hexdisplaytext;
+            // copybutton.disabled = false;
+
         })
+
+
 };
 
 
